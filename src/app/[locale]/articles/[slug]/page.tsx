28896e2adc7f309628a97a2163d694fb622af31b
tkugimot <toshimitsu_kugimoto@yahoo.co.jp>
2023-10-13 00:07:10 +0900

    Static export

--- src/app/[locale]/articles/[slug]/page.tsx
+++ src/app/[locale]/articles/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next'
-import { getDetail } from '@/libs/microcms'
+import { getDetail, getList } from '@/libs/microcms'
 import ArticlePage from '@/components/pages/ArticlePage'
+import { NUM_OF_PAGES_LIMIT } from '@/constants'
 
 type Props = {
   params: {
@@ -26,17 +27,18 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
           : `https://zubora-code.net/api/og?title=${data.title_ja}`,
       ],
     },
-  　twitter: {
-    　card: 'summary_large_image',
-    　title: params.locale === 'en' ? data.title_en : data.title_ja,
-    　description: params.locale === 'en' ? data.description_en : data.description_ja,
-    　creator: '@tkugimot',
-    　images: [
-       params.locale === 'en'
+    twitter: {
+      card: 'summary_large_image',
+      title: params.locale === 'en' ? data.title_en : data.title_ja,
+      description:
+        params.locale === 'en' ? data.description_en : data.description_ja,
+      creator: '@tkugimot',
+      images: [
+        params.locale === 'en'
           ? `https://zubora-code.net/api/og?title=${data.title_en}`
           : `https://zubora-code.net/api/og?title=${data.title_ja}`,
-     ],
-  　},
+      ],
+    },
   }
 }
 
@@ -46,5 +48,17 @@ export default async function Page({ params }: Props) {
 }
 
 export async function generateStaticParams() {
-  return [{ lang: 'ja' }, { lang: 'en' }]
+  const data = await getList({
+    limit: NUM_OF_PAGES_LIMIT,
+  })
+
+  const retList: { locale: string; slug: string }[] = []
+  data.contents.forEach((article) => {
+    retList.push(
+      { locale: 'ja', slug: article.id },
+      { locale: 'en', slug: article.id }
+    )
+  })
+
+  return retList
 }
